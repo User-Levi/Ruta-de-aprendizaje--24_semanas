f5b8ec0b997e07d45a2b348dec077db871a7351b
Levi <levi.progarming.213@gmail.com>
2026-03-27 10:48:33 -0600

    Day 1 / 2 finished

---
+++ 1_1_0_Monday.py
@@ -0,0 +1,33 @@
+# Day One:
+#   Variables
+#   Type data
+#   Operadores aritmeticos
+#   Operadores lógicos
+
+string = "cadena de texto"
+number = 5
+float = 5.5
+boolean = True
+diccionario = {
+    "a" : "dato uno",
+}
+set_s = {1,2,3,4,3}
+lista = [1,2,3]
+tupla = (1,2,3,4,5)
+
+
+#   Operadores aritméticos
+
+suma = 5 + 5
+resta = 5 - 5
+multiplicación = 5 * 5
+division = 3 / 9 # 3.3333333...
+wwwwentero = 3 // 9 # 3
+modulo = 10 % 3 # 1
+potencia = 2 ** 3 # 8
+
+#   Operadores lógicos
+
+y = True and False # if True and False is True
+o = True or False #if True or False is True
+not True # True >>>: False
---
+++ 1_2_0_Tuesday.py
@@ -0,0 +1,29 @@
+#Strings:
+
+name = "Levis"
+greeting = "Buen día"
+
+# f"{String}"
+
+print(f"¡{greeting} {name}!")
+
+# listas
+
+texto = "hola mundo"
+
+#   text
+print(texto.upper()) # Mayúsculas
+
+print(texto.capitalize())
+print(texto.replace("hola", "Adiós"))
+print(texto.split(" ")) # convierte en lista
+
+#       Slicing
+
+print(texto[:4])
+
+
+#   Tuple / List
+tupla = () # Inmutable
+lista = [] # Mutable
+
